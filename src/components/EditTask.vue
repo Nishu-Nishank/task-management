e294afc24ac450c97dae64ad1b196548cfd608fa
Nishu Nishank <nishank.nishu85@gmail.com>
2023-09-29 16:46:28 +0530

    Update EditTask.vue

--- src/components/EditTask.vue
+++ src/components/EditTask.vue
@@ -6,7 +6,7 @@
           <div>
             <span class="pr-2">Title:</span>
             <input
-              v-model="updateData.title"
+              v-model="updateTaskData.task.title"
               placeholder="Title"
               class="border pl-2 rounded-md ml-[58px] py-1"
             />
@@ -14,7 +14,7 @@
           <div class="flex items-center mt-5">
             <span>Description:</span>
             <textarea
-              v-model="updateData.description"
+              v-model="updateTaskData.task.description"
               placeholder="Description"
               class="border pl-2 rounded-md ml-4"
             ></textarea>
@@ -23,6 +23,7 @@
             <span>Upload Files:</span>
             <v-file-input
               ref="file"
+              v-model="updateTaskData.task.attachment"
               @change="uploadFile"
               @click:clear="removeAttachment"
               label="File input"
@@ -32,14 +33,14 @@
             <span>Select Date:</span>
             <input
               type="date"
-              v-model="updateData.estimatedTime"
+              v-model="updateTaskData.task.estimatedTime"
               class="border ml-4 rounded py-1 px-1"
             />
           </div>
           <div class="flex space-x-2 items-center mt-4">
             <span>Select Board:</span>
             <v-autocomplete
-              v-model="updateData.board"
+              v-model="updateTaskData.task.board"
               label="Select Board"
               :items="['Pending', 'Processing', 'Done']"
             ></v-autocomplete>
@@ -48,7 +49,7 @@
         </div>
         <div class="flex justify-center">
           <button
-            @click="createTask"
+            @click="updateTask(updateData)"
             class="px-4 py-1 border rounded-md bg-emerald-300 font-semibold"
           >
             Update Task
@@ -63,7 +64,7 @@
 </template>
 <script>
 import { VDatePicker } from "vuetify/labs/VDatePicker";
-import { reactive, ref } from "vue";
+import { reactive, ref, defineEmits } from "vue";
 import { useAppStore } from "../store/app";
 export default {
   components: {
@@ -73,36 +74,65 @@ export default {
     updateData: Object,
     closeModal: Function,
   },
-  setup() {
+   emits :['closeModal'],
+  setup({ updateData }, { emit }) {
+
     const store = useAppStore();
+    const taskBoardData = reactive({
+      pending: store.getPendingData,
+      processing: store.getProcessingData,
+      done: store.getDoneData,
+    });
     const updateTaskData = reactive({
       file: ref(null),
       dialog: true,
       task: {
-        pending: store.getPendingData,
-        processing: store.getProcessingData,
-        done: store.getDoneData,
+        title: updateData.title,
+        description: updateData.description,
+        attachment: updateData.attachment,
+        estimatedTime: updateData.estimatedTime,
+        board: updateData.board,
       },
     });
     const uploadFile = (event) => {
-        updateData.attachment.push(event.target.files[0]);
-    };
-    const deleteAttachment = () => {
-      updateTaskData.task.attachment = [];
+      updateData.attachment.push(event.target.files[0]);
     };
     const removeAttachment = () => {
       updateTaskData.task.attachment = null;
     };
-    const createTask = () => {
-
+    const updateTask = (ele) => {
+      const newData = {};
+      Object.keys(taskBoardData).forEach((key) => {
+        newData[key] = taskBoardData[key].filter((item) => isEqual(item, ele));
+      });
+      function isEqual(objA, objB) {
+        if (JSON.stringify(objA) === JSON.stringify(objB)) {
+          return (
+            (objA.attachment = updateTaskData.task.attachment),
+            (objA.board = updateTaskData.task.board),
+            (objA.description = updateTaskData.task.description),
+            (objA.estimatedTime = updateTaskData.task.estimatedTime),
+            (objA.title = updateTaskData.task.title)
+          );
+        }
+      }
+      store.updatePendingData(newData.pending);
+      store.updateProcessingData(newData.processing);
+      store.updateDoneData(newData.done);
+      emit('closeModal')
     };
+    const closeModal = ()=>{
+      emit('closeModal')
+    }
     return {
+      emit,
+      closeModal,
       updateTaskData,
       store,
+      taskBoardData,
+      updateTask,
       uploadFile,
       removeAttachment,
-      deleteAttachment,
-      createTask,
     };
   },
 };
